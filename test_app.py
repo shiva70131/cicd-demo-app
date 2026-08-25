3852352dbac0016b2be9fea4d2f1bec50fcca050
shiva70131 <pillikumar40@gmail.com>
2026-08-25 15:24:41 +0530

    commit changes in test-app.py file

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_home():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Hello" in response.data
+   assert b "WRONG TEXT" in response.data
